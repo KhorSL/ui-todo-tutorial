154fd7576644b6588c174ac9489667e381310f54
KhorSL <stephenksl@gmail.com>
2020-09-23 17:33:34 +0800

    Updates

---
+++ precache-manifest.5845dce567606d4de8a5520445f780ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3bd97be3e8b5e9e43d5b5ad39d7df50",
+    "url": "/ui-todo-tutorial/index.html"
+  },
+  {
+    "revision": "6e9a9affc33ef7823a06",
+    "url": "/ui-todo-tutorial/static/css/main.d2ba111d.chunk.css"
+  },
+  {
+    "revision": "994f17667ca6093b2d27",
+    "url": "/ui-todo-tutorial/static/js/2.0c0397c6.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/ui-todo-tutorial/static/js/2.0c0397c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e9a9affc33ef7823a06",
+    "url": "/ui-todo-tutorial/static/js/main.fb6e7217.chunk.js"
+  },
+  {
+    "revision": "ff9ff1abb2c1f2c1bfcf",
+    "url": "/ui-todo-tutorial/static/js/runtime-main.86cf155b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ui-todo-tutorial/precache-manifest.19ae72d149c55b0cdb7502e76aa7a0ed.js"
+  "/ui-todo-tutorial/precache-manifest.5845dce567606d4de8a5520445f780ce.js"
 );
 
 self.addEventListener('message', (event) => {
